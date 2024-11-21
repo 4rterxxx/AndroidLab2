664f1289a51d7bcdbf1935bfa7a50014da609a75
4rterxxx <tryaskov.alex@yandex.ru>
2024-11-21 11:11:53 +0300

    first commit

---
+++ app/src/main/java/com/example/androidlab2/CustomAdapter.kt
@@ -0,0 +1,35 @@
+package com.example.androidlab2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+data class ImageAndText( val dataSetText: Int,val dataSetImage: Int)
+class CustomAdapter(private val dataSet: Array<ImageAndText>) :
+    RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.textView)
+        val imageView: ImageView = view.findViewById(R.id.imageView)
+    }
+
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.recycler_objects, viewGroup, false)
+        return ViewHolder(view)
+    }
+
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.textView.setText(dataSet[position].dataSetText)
+        viewHolder.imageView.setImageResource(dataSet[position].dataSetImage)
+    }
+
+
+    override fun getItemCount() = dataSet.size
+
+}
---
+++ app/src/main/java/com/example/androidlab2/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.androidlab2
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val dataset = arrayOf(ImageAndText(R.string.google, R.drawable.google),ImageAndText(R.string.twitch, R.drawable.twitch), ImageAndText(R.string.youtube, R.drawable.youtube))
+
+        val customAdapter = CustomAdapter(dataset)
+
+        val recyclerView: RecyclerView = findViewById(R.id.recycler_id)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = customAdapter
+
+    }
+}
